55c5ad542d5eaa5ddea1696a90faae25623b6420
Anthony <AnthonyAniobi>
2022-10-12 19:50:29 +0100

    added image download to json image downloader

--- modules/json_image_downloader.py
+++ modules/json_image_downloader.py
@@ -1,7 +1,27 @@
 import os
-from . import image_downloader, test_json
-# from modules.image_downloader import imageDownloader
-# from modules.test_json import map
+
+from PIL import Image
+import requests
+from io import BytesIO
+
+def imageDownloader(image_url:str, file_format:str='jpg', image_folder:str='images', image_name:str=None)->str|None:
+    response = requests.get(image_url)
+    if image_name is None:
+        image_name = image_url.split('/')[-1]
+        image_name = image_name.split('.')[0]
+    
+    if response.status_code == 200:
+        if not os.path.isdir(image_folder):
+            os.mkdir(image_folder)
+
+        img = Image.open(BytesIO(response.content))
+        img.save(f'{image_folder}/{image_name}.{file_format}')
+        print(f'Saved image as => {image_name} !')
+        return image_name
+
+    else:
+        print(f'could not download image {image_name} with url: {image_url}')
+  
 
 def jsonImageDownloader(json:dict, image_path:str='images', file_format:str='jpeg')->None:
     
@@ -19,8 +39,36 @@ def jsonImageDownloader(json:dict, image_path:str='images', file_format:str='jpe
                 __jsonScraper(data)
         else:
             if data.split('.')[0].lower() in ('png', 'jpg', 'jpeg'):
-                image_downloader.imageDownloader(data, file_format, image_path)
+                imageDownloader(data, file_format, image_path)
 
 
 if __name__ == "__main__":
-    jsonImageDownloader(test_json.map, 'test_images', '.png')
+
+    test_result:dict = {
+        'found': 123,
+        'resulsts': [
+            {
+    "images":[
+        "https://res.cloudinary.com/jobizil/image/upload/v1602768183/images/menus/x4cspjvzqn2qk76sjhiw.jpg","https://res.cloudinary.com/jobizil/image/upload/v1602768183/images/menus/xnurgo60mme1ewupfbin.jpg","https://res.cloudinary.com/jobizil/image/upload/v1602768184/images/menus/ovni4qwzzxbufpsurlsg.jpg"
+        ],"_id":"5f5eccf4e923d0aca3e7d442","menuname":"Pounded Yamm","description":"Yams that have been crushed or stirred to a creamy, dough - like consistency.Pounded yams are considered a fufu(a kind of starchy side dish) usually served with a stew or efo.","__v":0
+        },{
+    "images":[
+        "https://res.cloudinary.com/jobizil/image/upload/v1602768183/images/menus/x4cspjvzqn2qk76sjhiw.jpg","https://res.cloudinary.com/jobizil/image/upload/v1602768183/images/menus/xnurgo60mme1ewupfbin.jpg","https://res.cloudinary.com/jobizil/image/upload/v1602768184/images/menus/ovni4qwzzxbufpsurlsg.jpg"
+        ],"_id":"5f5eccf4e923d0aca3e7d441","menuname":"Soup cream","description":"a soup prepared using cream, light cream, half and half or milk as a key ingredient. Sometimes the dairy product is added at the end of the cooking process, such as after a cream soup has been puréed.","__v":0
+        },{
+    "images":[
+        "https://res.cloudinary.com/jobizil/image/upload/v1602768183/images/menus/x4cspjvzqn2qk76sjhiw.jpg","https://res.cloudinary.com/jobizil/image/upload/v1602768183/images/menus/xnurgo60mme1ewupfbin.jpg","https://res.cloudinary.com/jobizil/image/upload/v1602768184/images/menus/ovni4qwzzxbufpsurlsg.jpg"
+        ],"_id":"5f5eccf4e923d0aca3e7d445","menuname":"Egusi Soup","description":"A stew usually made with crayfish or other meat and thickened with ground melon seeds(egusi).","__v":0
+        },{
+    "images":[
+        "https://res.cloudinary.com/jobizil/image/upload/v1602768183/images/menus/x4cspjvzqn2qk76sjhiw.jpg","https://res.cloudinary.com/jobizil/image/upload/v1602768183/images/menus/xnurgo60mme1ewupfbin.jpg","https://res.cloudinary.com/jobizil/image/upload/v1602768184/images/menus/ovni4qwzzxbufpsurlsg.jpg"
+        ],"_id":"5f5eccf4e923d0aca3e7d443","menuname":"Pepper Soups","description":"Light and spicy soup traditionally made with goat meat, but often with fish or other meat, as well as herbs and Nigerian spices.","__v":0
+        },{
+    "images":[
+        "https://res.cloudinary.com/jobizil/image/upload/v1602768183/images/menus/x4cspjvzqn2qk76sjhiw.jpg","https://res.cloudinary.com/jobizil/image/upload/v1602768183/images/menus/xnurgo60mme1ewupfbin.jpg","https://res.cloudinary.com/jobizil/image/upload/v1602768184/images/menus/ovni4qwzzxbufpsurlsg.jpg"
+        ],"_id":"5f5eccf4e923d0aca3e7d444","menuname":"Efo Riro","description":"A rich spinach stew usually made with spinach, scotch bonnets, and red bell peppers.","__v":0
+        }
+        ]
+    }
+
+    jsonImageDownloader(test_result, 'test_images', '.png')

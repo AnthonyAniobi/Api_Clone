06c3609389273a41d807942dbb0ccbec3e1bec11
Anthony <AnthonyAniobi>
2022-10-13 23:34:34 +0100

    setup v0.0.2
    - added documentation
    - make saveImage method a function
    - add project dependencies to pyproject.toml file
    - change version code

--- src/api_clone/api_downloader.py
+++ src/api_clone/api_downloader.py
@@ -1,3 +1,4 @@
+from enum import Enum
 from io import BytesIO
 from PIL import Image
 import json
@@ -8,13 +9,13 @@ import requests
 class ApiDownloader:
 
     map:dict
-    image_folder:str
+    folder_name:str
     image_format:str
     view_log:bool=True
 
-    def __init__(self, response:str, image_format:str='jpg', image_folder:str='images') -> None:
+    def __init__(self, response:str, image_format:str='jpg', folder_name:str='images') -> None:
         self.map = json.loads(response)
-        self.image_folder = image_folder
+        self.folder_name = folder_name
         self.image_format = image_format
 
     def __isImage(self, image:str)->bool:
@@ -31,7 +32,8 @@ class ApiDownloader:
         else:
             if type(data) is str:
                 if self.__isImage(data):
-                    self.saveImage(data)
+                    saveImage(data, self.folder_name, self.image_format, self.view_log)
+
 
     def __jsonRecurseImageDownloaderAtKey(self, data:dict|list|str, key_name:str):
         if type(data) is list:
@@ -40,7 +42,7 @@ class ApiDownloader:
         elif type(data) is dict:
             if key_name in data.keys():
                 if self.__isImage(data[key_name]):
-                    self.saveImage(data[key_name])
+                    saveImage(data[key_name], self.folder_name, self.image_format, self.view_log)
                 else:
                     print(f'value of {key_name}: is not an image')
             else:
@@ -58,31 +60,17 @@ class ApiDownloader:
             image_name = image_name.split('.')[0]
         
         if response.status_code == 200:
-            if not os.path.isdir(self.image_folder):
+            if not os.path.isdir(self.folder_name):
                 if self.view_log:
-                    print(f'creating folder => {self.image_folder}')
-                os.mkdir(self.image_folder)
+                    print(f'creating folder => {self.folder_name}')
+                os.mkdir(self.folder_name)
             img = Image.open(BytesIO(response.content))
-            img.save(f'{self.image_folder}/{image_name}.{self.image_format}')
+            img.save(f'{self.folder_name}/{image_name}.{self.image_format}')
             if self.view_log:
                 print(f'Saved image as => {image_name} !')
         else:
             if self.view_log:
                 print(f'Couldn\'t get image \nCheck your internet connection!')
-
-    def downloadAllImages(self, folder_name:str|None=None):
-        '''Scrape a json file and download all images recursively'''
-        if self.view_log:
-            print('downloading all images in api')
-
-        __localFolder:str
-        if folder_name:
-            __localFolder = self.image_folder #store initial value of folder
-            self.image_folder = folder_name
-        self.__jsonRecursiveImageDownloader(self.map)
-        self.image_folder = __localFolder #return the folder to initial value
-        if self.view_log:
-            print('Completed!')
 
     def downloadImagesAtKey(self, key_name:str, folder_name:str|None=None):
         '''Scrape a json file and download all images with the key_name you specify'''
@@ -91,12 +79,37 @@ class ApiDownloader:
         
         __localFolder:str
         if folder_name:
-            __localFolder = self.image_folder #store initial value of folder
-            self.image_folder = folder_name
+            __localFolder = self.folder_name #store initial value of folder
+            self.folder_name = folder_name
         self.__jsonRecurseImageDownloaderAtKey(self.map, key_name)
-        self.image_folder = __localFolder #return the folder to initial value
+        self.folder_name = __localFolder #return the folder to initial value
         if self.view_log:
             print('Completed!')
 
+
+class ImageFormat(Enum):
+    PNG='png'
+    JPG='jpg'
+    JPEG='jpeg'
+
+def saveImage(image_url:str, folder_name:str='images', image_format:ImageFormat=ImageFormat.JPG, view_log:bool=True, image_name:str|None=None)->str|None:
+    '''Save an image from Url'''
+    response = requests.get(image_url)
     
+    if image_name is None:
+        image_name = image_url.split('/')[-1]
+        image_name = image_name.split('.')[0]
+    
+    if response.status_code == 200:
+        if not os.path.isdir(folder_name):
+            if view_log:
+                print(f'creating folder => {folder_name}')
+            os.mkdir(folder_name)
+        img = Image.open(BytesIO(response.content))
+        img.save(f'{folder_name}/{image_name}.{image_format}')
+        if view_log:
+            print(f'Saved image as => {image_name} !')
+    else:
+        if view_log:
+            print(f'Couldn\'t get image \nCheck your internet connection!')
         

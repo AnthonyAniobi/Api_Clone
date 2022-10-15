517bb6fa3d972f8f9be266d79277748d55078db7
Anthony <AnthonyAniobi>
2022-10-15 04:25:31 +0100

    added method to remove items from json

--- src/api_clone/api_refactor.py
+++ src/api_clone/api_refactor.py
@@ -13,24 +13,30 @@ class ApiRefactor:
         if type(data) is dict:
             if key_name in data.keys():
                 data.pop(key_name)
-                return data
+                # return data
             else:
                 for key, val in data.items():
-                    return self.__recursiveRename(val, key_name)  
+                    data[key] = self.__recursiveRename(val, key_name)
+                    # return {key:  self.__recursiveRename(val, key_name)}  
         elif type(data) is list:
-            for val in data:
-                return self.__recursiveRename(val, key_name)
+            for index in range(len(data)):
+                data[index] = self.__recursiveRename(data[index], key_name)
         else:
-            return data
+            pass
+            # return data
 
     def removeField(self, key_name:str)->dict:
-        '''remove a key with name'''
-        return self.__recursiveRename(map, key_name)
+        '''remove every occurence of a key with the key_name specified'''
+        temp_data = self.map.copy()
+        self.__recursiveRename(temp_data, key_name)
+        return temp_data
 
 
 if __name__ == '__main__':
-    json_map = {"some": 23, "test": "some test", "values": 32}
+    json_map = {"some": 23, "test": "some_test", "values": 32}
     json_data = json.dumps(json_map)
     api_refactor = ApiRefactor(json_data)
     result = api_refactor.removeField("test")
     print(json_data)
+    print (api_refactor.map)
+    print(result)

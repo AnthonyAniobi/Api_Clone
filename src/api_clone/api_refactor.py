1dc8e5852576fcf48cd2bf6382dc96045a2f3cef
Anthony <AnthonyAniobi>
2022-10-15 04:26:13 +0100

    refactor and rename methods

--- src/api_clone/api_refactor.py
+++ src/api_clone/api_refactor.py
@@ -9,26 +9,23 @@ class ApiRefactor:
     def __init__(self, response:str) -> None:
         self.map = json.loads(response)
 
-    def __recursiveRename(self, data:dict|list|str, key_name:str):
+    def __recursiveRemove(self, data:dict|list|str, key_name:str):
         if type(data) is dict:
             if key_name in data.keys():
                 data.pop(key_name)
-                # return data
             else:
                 for key, val in data.items():
-                    data[key] = self.__recursiveRename(val, key_name)
-                    # return {key:  self.__recursiveRename(val, key_name)}  
+                    data[key] = self.__recursiveRemove(val, key_name)
         elif type(data) is list:
             for index in range(len(data)):
-                data[index] = self.__recursiveRename(data[index], key_name)
+                data[index] = self.__recursiveRemove(data[index], key_name)
         else:
             pass
-            # return data
 
     def removeField(self, key_name:str)->dict:
         '''remove every occurence of a key with the key_name specified'''
         temp_data = self.map.copy()
-        self.__recursiveRename(temp_data, key_name)
+        self.__recursiveRemove(temp_data, key_name)
         return temp_data
 
 
